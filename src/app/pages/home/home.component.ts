e07826fcde1141bf83cbc84cf99b5594550b2389
BrianKawinzi <brian.mutisya@starthmore.edu>
2023-08-06 16:27:52 +0300

    styling the search card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -20,7 +20,7 @@ export class HomeComponent {
       {
         image:'https://www.grxstatic.com/d4fuqqd5l3dbz/products/cwf_tms/DrugItem_15029.PNG?width=384&quality=75',
         name: 'Atorvastatin',
-        description: '40mg, 30 tablets',
+        details: '40mg, 30 tablets',
         originalPrice: '59.99',
         discountedPrice: '40',
         discountDetails: '98% off with MedRx'
@@ -29,14 +29,14 @@ export class HomeComponent {
       {
         image:'https://buzzrx.s3.amazonaws.com/gs/DrugItem_5059.JPG',
         name: 'Metformin',
-        description: '500mg, 60 tablets',
+        details: '500mg, 60 tablets',
         originalPrice: '24.99',
         discountedPrice: '0.75',
         discountDetails: '97% off with MedRx'
       },
       {image:'https://buzzrx.s3.amazonaws.com/gs/DrugItem_5059.JPG',
         name: 'Lisinopril',
-        description: '20mg, 30 tablets',
+        details: '20mg, 30 tablets',
         originalPrice: '$29.99',
         discountedPrice: '19',
         discountDetails: '95% off with MedRx'
@@ -44,7 +44,7 @@ export class HomeComponent {
       {
         image:'https://buzzrx.s3.amazonaws.com/caf6e1e8-8e0e-45c3-840d-c10080913b0e/lisinopril-img.jpeg',
         name: 'Lisinopril',
-        description: '20mg, 30 tablets',
+        details: '20mg, 30 tablets',
         originalPrice: '29.99',
         discountedPrice: '19.22',
         discountDetails: '95% off with MedRx'
@@ -52,7 +52,7 @@ export class HomeComponent {
       {
         image:'https://buzzrx.s3.amazonaws.com/caf6e1e8-8e0e-45c3-840d-c10080913b0e/lipitor-img.jpeg',
         name: 'Lipitor ',
-        description: '20mg, 30 tablets',
+        details: '20mg, 30 tablets',
         originalPrice: '69.99',
         discountedPrice: '50',
         discountDetails: '95% off with MedRx'
